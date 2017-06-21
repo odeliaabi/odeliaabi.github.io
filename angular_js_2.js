af4f9d05f1db4eb8b84b26d511fb62acfccb830f
DESKTOP-B6OLFLB\abizmil odelia <odelia.abizmil@gmail.com>
2017-06-21 16:39:09 +0200

    version 1

---
+++ angular_js_2.js
@@ -0,0 +1,24 @@
+  function commentsCtrl($scope){
+      $scope.comments= [
+      {"username":"John Smith",
+      "city":"New York",
+      "mail":"john.smith@gmail.com",
+      "content":"JS is cool!!"},
+      {"username":"David Cohen",
+      "city":"Tel Aviv",
+      "mail":"david.co@gmail.com",
+      "content":"Angular JS is a framework very interesting"},
+      {"username":"Max Reinler",
+      "city":"Paris",
+      "mail":"max@gmail.com",
+      "content":"Yes of couse! but it can be complicated for the beginners"},
+      {"username":"Uzalara Khartigesu",
+      "city":"Bangkok",
+      "mail":"khartigesu@gmail.com",
+      "content":"I' m a beginer in JS and I find it more comprensible with youtube videos !"},
+      {"username":"Emmanuel Nguyen",
+      "city":"Pekin",
+      "mail":"nguyen@gmail.com",
+      "content":"I am agree with uzalara ! See my tuto on you tube "}
+      ]
+    }
